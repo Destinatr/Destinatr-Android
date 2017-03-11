b72e01332bc5ded5f336f35e6578c0cce33ecfca
SirKhepre <brandonroberge@hotmail.ca>
2017-03-11 01:58:19 -0500

    Added stuff

--- app/src/main/java/io/lassondehacks/destinatr/MapsActivity.kt
+++ app/src/main/java/io/lassondehacks/destinatr/MapsActivity.kt
@@ -1,7 +1,9 @@
 package io.lassondehacks.destinatr
 
+import android.content.pm.PackageManager
 import android.support.v4.app.FragmentActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +11,34 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
+import android.app.AlertDialog
+import android.location.Location
+import android.location.LocationListener
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdate
+import kotlinx.android.synthetic.main.activity_maps.*
+import android.R.string.cancel
+import android.content.DialogInterface
+import android.content.res.ColorStateList
 
-class MapsActivity : FragmentActivity(), OnMapReadyCallback {
+
+class MapsActivity : FragmentActivity(),
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    val REQUEST_LOCATION_PERMISSION = 1
 
     private var mMap: GoogleMap? = null
+
+    var googleApiClient: GoogleApiClient? = null
+    var mLastLocation: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,24 +47,108 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        search_bar.setOnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.x <= search_bar.compoundDrawables[DRAWABLE_LEFT].bounds.width()) {
+                    showParkingFiltersAlert()
+                    return@setOnTouchListener true
+                }
+            }
+            return@setOnTouchListener false
+        }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        } else {
+            setupMap()
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == REQUEST_LOCATION_PERMISSION) {
+                setupMap()
+            }
+        }
+    }
+
+    fun setupMap() {
+        mMap!!.isMyLocationEnabled = true
+        mMap!!.setPadding(0, 250, 0, 0)
+    }
+
+    override fun onStart() {
+        if (googleApiClient == null) {
+            googleApiClient = GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build()
+        }
+        googleApiClient?.connect()
+        super.onStart()
+    }
+
+    override fun onStop() {
+        googleApiClient?.disconnect()
+        super.onStop()
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        } else {
+            updatePosition()
+
+            var currentPosition = mLastLocation
+            val center = CameraUpdateFactory.newLatLng(LatLng(currentPosition!!.latitude, currentPosition!!.longitude))
+            mMap!!.moveCamera(center)
+            mMap!!.animateCamera(CameraUpdateFactory.zoomTo(15f))
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        updatePosition()
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+    }
+
+    fun updatePosition() {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient)
+    }
+
+    fun showParkingFiltersAlert() {
+        var parkingFilters = AlertDialog.Builder(this)
+        parkingFilters.setTitle("Filtres de stationnements")
+
+        parkingFilters.setView(R.layout.parking_filters)
+        parkingFilters.setPositiveButton("Fermer", DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+        var alert = parkingFilters.create()
+        alert.show()
+    }
+
 }
