53554bea04cadffdabaa461722af9dea1935fde3
Richer Archambault <richer.arc@gmail.com>
2017-03-11 01:41:52 -0500

    quick fix

--- app/src/main/java/io/lassondehacks/destinatr/MainActivity.kt
+++ app/src/main/java/io/lassondehacks/destinatr/MainActivity.kt
@@ -13,15 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var res1 = Result("Premier resultat", "une adresse", 10)
-        var res2 = Result("Deuxieme resultat", "une adresse", 101)
-        var res3 = Result("troisieme resultat", "une adresse", 1001)
-
-        var results = arrayOf(res1, res2, res3)
-
-        var ft = supportFragmentManager.beginTransaction()
-        ft.add(R.id.activity_main, ResultListViewFragment(results, { selectDest() }))
-        ft.commit()
+//        var res1 = Result("Premier resultat", "une adresse", 10)
+//        var res2 = Result("Deuxieme resultat", "une adresse", 101)
+//        var res3 = Result("troisieme resultat", "une adresse", 1001)
+//
+//        var results = arrayOf(res1, res2, res3)
+//
+//        var ft = supportFragmentManager.beginTransaction()
+//        ft.add(R.id.activity_main, ResultListViewFragment(results, { selectDest() }))
+//        ft.commit()
     }
 
     fun selectDest() {
